42ab896c5eb946c19213dc05927f52d1e0314400
Hagar Elmadany <hagarelmadany@gmail.com>
2025-03-09 22:56:10 +0200

    Add posts table migration file

---
+++ database/migrations/2025_03_09_132840_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //table (id,title(varchhar),description(TEXT),createdat (timedate))
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', length: 100);   // = varchar
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
