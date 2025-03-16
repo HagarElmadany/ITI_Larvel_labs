9bdefcb38d16a1547f2649aca205dbe5823e2acb
Hagar Elmadany <hagarelmadany@gmail.com>
2025-03-16 11:59:14 +0200

    schedule PruneOldPostsJob to run daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->job(new \App\Jobs\PruneOldPostsJob)->dailyAt('00:00');
         // $schedule->command('inspire')->hourly();
     }
 
