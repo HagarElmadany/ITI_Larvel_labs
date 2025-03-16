e42f02d7d3ca0a40960e26e7fb96c4e3d0cf6e8f
Hagar Elmadany <hagarelmadany@gmail.com>
2025-03-16 07:53:38 +0200

    Add authentication middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,21 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
+Route::middleware(['auth'])->get('/posts', [PostsController::class, 'index'])->name('posts.index');
 
 
-Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+Route::middleware(['auth'])->get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
 
-Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+Route::middleware(['auth'])->post('/posts', [PostsController::class, 'store'])->name('posts.store');
 
-Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
+Route::middleware(['auth'])->get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
 
 
-Route::delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
+Route::middleware(['auth'])->delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
 
-Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
+Route::middleware(['auth'])->get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
 
-Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
+Route::middleware(['auth'])->put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
 
 
 
